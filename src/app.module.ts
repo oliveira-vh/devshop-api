e0fb3c7fb7fd3a7826125e6543496434f6751a97
Victor de Oliveira <oliveira.wn@gmail.com>
2021-03-02 20:32:45 -0300

    category

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Category } from './category/category.entity';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+    /*
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://postgres:abc123@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true,
+      //entities: [Category],
+      logging: true,
+    }),
+    */
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/category/category.resolver.ts
@@ -0,0 +1,12 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { CategoryService } from './category.service';
+import { CategoryPublic } from './dto/category';
+
+@Resolver(of => CategoryPublic)
+export class CategoryResolver {
+  constructor(private readonly categoryService: CategoryService) {}
+  @Query(returns => [CategoryPublic], { name: 'getAllCategories' })
+  async getAllCategories(): Promise<CategoryPublic[]> {
+    return await this.categoryService.findAll();
+}
+}
